a026744e241550ac9a92fd1e327008bbee15a46d
Grawi <82295508+Grawi@users.noreply.github.com>
2023-10-30 13:10:07 +0800

    add analysis code

--- data_analysis/utils.py
+++ data_analysis/utils.py
@@ -1,3 +1,58 @@
+import pandas
+import numpy as np
+import matplotlib.pyplot
+import pandas as pd
+import datetime
+
+human_index = 1
+
+def sliding_average(x, y, window_half_width=0.75, window_slide=0.1):
+    sliding_centers = np.arange(x.min(), x.max(), window_slide)
+    means = pandas.Series(index=sliding_centers, data=[
+        y[x.between(sliding_center - window_half_width, sliding_center + window_half_width, inclusive='right')].mean()
+        for sliding_center in sliding_centers
+    ]).rename(y.name)
+    stds = pandas.Series(index=sliding_centers, data=[
+        y[x.between(sliding_center - window_half_width, sliding_center + window_half_width, inclusive='right')].std()
+        for sliding_center in sliding_centers
+    ]).rename(y.name)
+    return [means, means - stds, means + stds]
+
+
+
+def savefig(fig, filename, hide_top_spine=True, hide_right_spine=True, transparent=True, dpi=100, tight_layout=True):
+    if tight_layout:
+        fig.tight_layout()
+    for ax in fig.axes:
+        ax.spines['top'].set_visible(not hide_top_spine)
+        ax.spines['right'].set_visible(not hide_right_spine)
+    fig.savefig(filename, transparent=transparent, dpi=dpi)
+
+
+
+def plot_with_std(ax, average: pandas.Series, low: pandas.Series, high: pandas.Series):
+    ax.plot(average.index, average.values, lw=5, alpha=0.7)
+    ax.fill_between(average.index, low, high, alpha=0.2)
+
+
+
+def get_datetime_str():
+    return f'{datetime.datetime.today():%Y_%m_%d}'
+
+
+
+def load_data(conditions, path="data/"):
+    df = None
+    if isinstance(conditions, int):
+        df = pd.read_csv(path + f"condition_{conditions}.csv")
+    elif isinstance(conditions, list):
+        for condition in conditions:
+            df = pd.concat([df, pd.read_csv(path + f"condition_{condition}.csv")])
+    else:
+        raise ValueError
+    return df
+
+
 phase_1_statements = [
     {
         'statement': "Government regulation of business is necessary to protect the public interest.",
@@ -88,3 +143,13 @@ phase_1_statements = [
         'range': ['Wet', 'Dry']
     },
 ]
+
+question_title_map = {
+    "all": "All questions",
+    0: "\"Invest more into space exploration\" (issue)",
+    1: "\"Approve cloning\" (issue)",
+    2: "\"Population growth\" (fact)",
+    3: "\"Cell production\" (fact)",
+    4: "\"Erasing memory brings happiness\" (prediction)",
+    5: "\"Robots make efficiency\" (prediction)"
+}
