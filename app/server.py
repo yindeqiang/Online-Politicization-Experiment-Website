97613544170ad1e1de6c03bbf92ac891ea84f1cc
xrshen2002 <2100017479@stu.pku.edu.cn>
2024-08-20 19:32:05 +0800

    08201932

--- app/server.py
+++ app/server.py
@@ -50,7 +50,6 @@ if not app.config["DEBUG"]:
         __tablename__ = "pilot_1"
         ideology_answers = Column(JSON)
         additional_answers = Column(JSON)
-        driven_answers = Column(JSON)
 
     class Pilot_2(Base):
         __tablename__ = "pilot_2"
@@ -151,8 +150,6 @@ def quiz(quiz_type):
                     bot_detected=post_data.get('bot_detected'),
                     submit_time=datetime.now(),
                     reason=post_data.get("reason"),
-                    driven_answers=post_data.get("driven_answers"),
-
                 )
                 db.session.add(pilot_1_data)
 
