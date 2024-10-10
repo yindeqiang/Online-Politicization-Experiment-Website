c2510536fe9e29226def456872ac598889c56e0e
尹德强 <topcg@163.com>
2024-10-10 14:15:24 +0800

    feat: 插入新的环节

--- app/static/js/script_phase_0.js
+++ app/static/js/script_phase_0.js
@@ -1,5 +1,9 @@
 phase = 0;
 
 function init_phase_0() {
-    console.log(1111);
+    const body_string = `
+        <h1 class="questions_block_title"></h1>
+        <div class="questions_list"></div>
+    `;
+    document.querySelector(".quiz_body").innerHTML = body_string;
 }
