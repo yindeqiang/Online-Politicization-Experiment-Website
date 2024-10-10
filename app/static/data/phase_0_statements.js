d9812c00d7338d899551aa2d3807364d884f9805
尹德强 <topcg@163.com>
2024-10-10 15:30:25 +0800

    feat: dev

---
+++ app/static/data/phase_0_statements.js
@@ -0,0 +1,108 @@
+const phase_1_statements = [
+    [
+        {
+            index: 0,
+            text: 'How many homicides were officially registered in Chicago in 2021?',
+            range: [0, 100],
+            axis: 'number',
+            suffix: ''
+        }, 
+        {
+            index: 1,
+            text: 'What was the percentage of the population growth in California from 2010 to 2020?',
+            range: [0, 0.1],
+            axis: 'percentage',
+            suffix: ''
+        }, 
+        {
+            index: 2,
+            text: 'How far on average is Mars from Earth?',
+            range: [100, 200],
+            axis: 'number',
+            suffix: 'million miles'
+        }, 
+        {
+            index: 3,
+            text: '.How many cells on average does an adult body produce every second?',
+            range: [20, 30],
+            axis: 'number',
+            suffix: 'million'
+        }, 
+        {
+            index: 4,
+            text: 'How many elevators are there in New York’s Empire State Building?',
+            range: [70, 80],
+            axis: 'number',
+            suffix: ''
+        }, 
+        {
+            index: 5,
+            text: 'How long in minutes approximately are all 8 Harry Potter movies?',
+            range: [1100, 1300],
+            axis: 'number',
+            suffix: ''
+        }
+    ],
+    [
+        {
+            index: 6,
+            text: 'If robots and computers were able to perform most of the jobs currently being done by humans, the economy as a whole would be more efficient.',
+            axis: 'attitude'
+        },
+        {
+            index: 7,
+            text: 'If scientists invented a non-invasive clinical surgery that could accurately erase people’s memory of a certain period, the overall happiness of our society would be greatly improved.',
+            axis: 'attitude'
+        },
+        {
+            index: 8,
+            text: 'Within the next 100 years, human beings will have contact with intelligent life from other planets.',
+            axis: 'attitude'
+        },
+        {
+            index: 9,
+            text: 'Very likely, a breakthrough will occur within the next 5 years in the area of cheaper energy.',
+            axis: 'attitude'
+        },
+        {
+            index: 10,
+            text: 'Within 50 years, cryptocurrencies (such as Bitcoin, Ethereum, or Litecoin) will replace the US dollars and become the official currency of the US.',
+            axis: 'attitude'
+        },
+        {
+            index: 11,
+            text: 'Within 50 years, self-driving technology will be sufficiently mature, so that most personal cars will no longer be equipped with a steering wheel.',
+            axis: 'attitude'
+        },
+        {
+            index: 12,
+            text: 'The government should invest more scientific research funding into space exploration instead of virtual reality technology.',
+            axis: 'attitude'
+        },
+        {
+            index: 13,
+            text: 'The use of cloning technology should be approved, to provide infertile couples using test-tube fertilization with more embryos to increase their chances of conceiving.',
+            axis: 'attitude'
+        },
+        {
+            index: 14,
+            text: 'Everyone should be allowed to use paid advertising to present their point of view on controversial public policy issues, no matter how insane it is.',
+            axis: 'attitude'
+        },
+        {
+            index: 15,
+            text: 'If newer technologies become available in the future related to vision or sight, it is appropriate to use technologies to improve a person\'s normal vision to a level that is greatly beyond normal human capabilities.',
+            axis: 'attitude'
+        },
+        {
+            index: 16,
+            text: 'If scientists discover that a huge asteroid is about to hit and destroy the earth in one year, the government should keep it secret from the public to avoid social panic.',
+            axis: 'attitude'
+        },
+        {
+            index: 17,
+            text: 'If technically feasible, adults without fertility should be allowed to adopt their own clones.',
+            axis: 'attitude'
+        }
+    ]
+]
--- app/static/js/script_phase_0.js
+++ app/static/js/script_phase_0.js
@@ -6,4 +6,9 @@ function init_phase_0() {
         <div class="questions_list"></div>
     `;
     document.querySelector(".quiz_body").innerHTML = body_string;
+
+    const title = document.querySelector('.questions_block_title');
+    const questions_list = document.querySelector('.questions_list');
+
+    
 }
